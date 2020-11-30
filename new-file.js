fa08eeb10e476f05371d88bd084187eaef76c5e1
Sergey Khramovich <ssd.hogan@gmail.com>
2020-11-30 19:03:47 +0300

    Upd

--- new-file.js
+++ new-file.js
@@ -3,5 +3,6 @@ console.log('lol js');
 console.log('lol js');
 console.log('lol js');
 console.log('lol js');
+console.log('Line for pr. reset();');
 console.log('lol js new file nnn');
 console.log('lol js');
